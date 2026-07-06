3d33aa1e781ea75717cd15bd978dd61a42601c7b
duckida <duckidaweb@gmail.com>
2026-07-06 20:16:48 +0100

    add basic frontend/backend interaction

--- JOURNAL.md
+++ JOURNAL.md
@@ -7,3 +7,8 @@ July 2: looked for parts online from a variety of resellers
 July 4: ordered parts on Amazon, used threading to make a basic Flask API to search for games, added make move function
  
 July 5: got a basic API working and was able to move pieces against another player with it! Learned the basics of Next.js and started making the frontend
+
+July 6: got basic interaction between the frontend & backend working!
+
+### AI usage
+I used AI to help me understand the LiChess API responses, choose some parts, and with learning Next.js / Node.
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,49 @@
 "use client";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
 
-export default function Page() {
+// backend calling functions
+const BASE_URL = "http://127.0.0.1:5000";
+
+function searchGame() {
+  axios.post(`${BASE_URL}/search-and-join-game`).catch((error) => {
+    console.error(error);
+  });
+}
+
+function startGame() {
+  axios.post(`${BASE_URL}/update-game`).catch((error) => {
+    console.error(error);
+  });
+}
+
+function resetGame() {
+  axios.post(`${BASE_URL}/reset-game`).catch((error) => {
+    console.error(error);
+  });
+}
+
+// custom ui components
+
+function StatusText() {
+  const [status, setStatus] = useState("No game active");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      axios.get(`${BASE_URL}/status`).then(function (response) {
+        setStatus(JSON.stringify(response.data));
+      });
+    }, 1000); // check every 1000 ms
+
+    return () => clearInterval(intervalId);
+  }, []);
+  return <h2>{status}</h2>;
+}
+
+function LiChessboard() {
   const chessboardOptions = {
     // your config options here
   };
@@ -14,10 +54,25 @@ export default function Page() {
 
   console.log(possibleMoves);
 
+  return <Chessboard options={chessboardOptions} />;
+}
+
+export default function Page() {
   return (
     <>
-      <h1>Hello, Next.js!</h1>
-      <Chessboard options={chessboardOptions} />
+      <div style={{ width: "50vw" }}>
+        <Button variant="outline" onClick={() => searchGame()}>
+          Search Game
+        </Button>
+        <Button variant="outline" onClick={() => startGame()}>
+          Start Game (once found)
+        </Button>
+        <Button variant="outline" onClick={() => resetGame()}>
+          Reset Game
+        </Button>
+        <StatusText />
+        <LiChessboard />
+      </div>
     </>
   );
 }
