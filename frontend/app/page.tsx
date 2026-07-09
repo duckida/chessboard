989cbfc3ca740df590aba5761c666e07d7399391
duckida <duckidaweb@gmail.com>
2026-07-09 20:23:46 +0100

    rename play_lichess, add links to buttons

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,24 +3,31 @@ import { useRef, useState, useEffect } from "react";
 import axios from "axios";
 import { Button } from "@/components/ui/button";
 import { Power } from "lucide-react"
+import Link from 'next/link';
 
 export default function Page() {
   return (
     <>
       <div className="flex flex-col gap-2 items-center p-5">
 
-        <h1 className="font-mono font-bold text-2xl">Chessboard</h1>
-        <Button className="w-[80vw] h-[20vh] bg-green-600 text-lg font-bold">
-          Play Stockfish
-        </Button>
+        <Link href="/play_stockfish">
+          <h1 className="font-mono font-bold text-2xl">Chessboard</h1>
+          <Button className="w-[80vw] h-[20vh] bg-green-600 text-lg font-bold">
+            Play Stockfish
+            </Button>
+        </Link>
 
-        <Button className="w-[80vw] h-[20vh] bg-yellow-800 text-lg font-bold">
-          Play LiChess
-        </Button>
+        <Link href="/play_lichess">
+          <Button className="w-[80vw] h-[20vh] bg-yellow-800 text-lg font-bold">
+            Play LiChess
+          </Button>
+        </Link>
 
-        <Button className="w-[80vw] h-[20vh] bg-sky-400 text-lg font-bold">
-          Human Play
-        </Button>
+        <Link href="/play_human">
+          <Button className="w-[80vw] h-[20vh] bg-sky-400 text-lg font-bold">
+            Human Play
+          </Button>
+        </Link>
       </div>
 
       <div>
