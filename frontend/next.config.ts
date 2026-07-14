422db205d85b048a3f1b3bf805cd7367f47eaa0d
duckida <duckidaweb@gmail.com>
2026-07-14 16:11:27 +0100

    Add static export config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
 };
 
 module.exports = {
