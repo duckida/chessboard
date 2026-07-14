18ffc77824d48300591cde2db1f2e61792a3f843
duckida <duckidaweb@gmail.com>
2026-07-14 12:57:57 +0100

    Fix strip inverted colors

--- backend/tests/neopixel_test.py
+++ backend/tests/neopixel_test.py
@@ -5,8 +5,8 @@ LED_COUNT = 8       # Number of LEDs
 LED_GPIO = 19
 LED_FREQ = 800000
 LED_DMA = 5
-LED_INVERT = True       # Invert required when using inverting buffer
-LED_BRIGHTNESS = 255
+LED_INVERT = False       # Invert required when using inverting buffer
+LED_BRIGHTNESS = 50
 
 strip = PixelStrip(LED_COUNT, LED_GPIO, LED_FREQ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, channel=1, strip_type=ws.WS2811_STRIP_GRB)
 strip.begin()
