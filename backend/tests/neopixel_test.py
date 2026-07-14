bf80647addd2ea7f4ec5e468e6b01529bb0c4966
duckida <duckidaweb@gmail.com>
2026-07-14 13:07:54 +0100

    Update strip brightness & color

--- backend/tests/neopixel_test.py
+++ backend/tests/neopixel_test.py
@@ -5,13 +5,13 @@ LED_COUNT = 8       # Number of LEDs
 LED_GPIO = 19
 LED_FREQ = 800000
 LED_DMA = 5
-LED_INVERT = False       # Invert required when using inverting buffer
-LED_BRIGHTNESS = 50
+LED_INVERT = False
+LED_BRIGHTNESS = 100
 
 strip = PixelStrip(LED_COUNT, LED_GPIO, LED_FREQ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, channel=1, strip_type=ws.WS2811_STRIP_GRB)
 strip.begin()
 
 for i in range(strip.numPixels()):
-    strip.setPixelColor(i, Color(255, 0, 0))
+    strip.setPixelColor(i, Color(139,69,19))
 
 strip.show()
