6e7497d66a9f700c4ff4702e4e0e3044eed9df07
duckida <duckidaweb@gmail.com>
2026-07-16 16:47:49 +0100

    Make it use 8x8 grid

--- pico_matrix_scan/main.py
+++ pico_matrix_scan/main.py
@@ -5,11 +5,12 @@ from time import sleep
 from keypad_lib import Keypad
 from machine import Pin
 
-ROW_GPIO_LIST = [Pin(1), Pin(0)]
-COL_GPIO_LIST = [Pin(2), Pin(3)]
+ROW_GPIO_LIST = [Pin(0), Pin(1), Pin(2), Pin(3), Pin(4), Pin(5), Pin(6), Pin(7)]
+COL_GPIO_LIST = [Pin(8), Pin(9), Pin(10), Pin(11), Pin(12), Pin(13), Pin(14), Pin(15)]
+
 keypad = Keypad(ROW_GPIO_LIST, COL_GPIO_LIST)
 
 while True:
     key_list = keypad.read_keypad()
     print(key_list)
-    sleep(0.05)  # debounce and delay
+    sleep(0.1)  # debounce and delay
