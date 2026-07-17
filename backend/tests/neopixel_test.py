6881c09ad0d2bb4b8e7faf11de63317935bddc1a
duckida <duckidaweb@gmail.com>
2026-07-17 20:33:11 +0100

    Setup for 64 NeoPixel test

--- backend/tests/neopixel_test.py
+++ backend/tests/neopixel_test.py
@@ -1,12 +1,12 @@
 from rpi_ws281x import PixelStrip, Color, ws
 import time
 
-LED_COUNT = 8       # Number of LEDs
+LED_COUNT = 64       # Number of LEDs
 LED_GPIO = 19
 LED_FREQ = 800000
 LED_DMA = 5
 LED_INVERT = False
-LED_BRIGHTNESS = 100
+LED_BRIGHTNESS = 50
 
 strip = PixelStrip(LED_COUNT, LED_GPIO, LED_FREQ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, channel=1, strip_type=ws.WS2811_STRIP_GRB)
 strip.begin()
