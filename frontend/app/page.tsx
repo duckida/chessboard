b632c36f29c0fd4b2b428c4864bfdf3bf89d15d5
duckida <duckidaweb@gmail.com>
2026-08-27 17:36:29 +0100

    improve touch friendliness

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -16,23 +16,23 @@ function shutdown() {
 export default function Page() {
   return (
     <>
-      <div className="flex flex-col gap-4 items-center p-5 w-[300px]">
+      <div className="flex flex-col gap-4 items-center p-5 w-[320px]">
         <h1 className="font-mono font-bold text-2xl">Chessboard</h1>
 
         <Link href="/play_stockfish">
-          <Button className="w-[300px] h-[100px] bg-green-600 text-lg font-bold">
+          <Button asChild className="w-[300px] h-[100px] bg-green-600 text-lg font-bold">
             Play Stockfish
             </Button>
         </Link>
 
         <Link href="/play_lichess">
-          <Button className="w-[300px] h-[100px] bg-yellow-800 text-lg font-bold">
+          <Button asChild className="w-[300px] h-[100px] bg-yellow-800 text-lg font-bold">
             Play LiChess
           </Button>
         </Link>
 
-        <Link href="/play_human">
-          <Button className="w-[300px] h-[100px] bg-sky-400 text-lg font-bold">
+        <Link href="/play_human" >
+          <Button asChild className="w-[300px] h-[100px] bg-sky-400 text-lg font-bold">
             Human Play
           </Button>
         </Link>
